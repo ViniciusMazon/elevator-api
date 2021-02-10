1b234ddcaccfbaa28c636e2c794f8d0b1cfb7a8e
Vinicius Mazon <vnpmazon@gmail.com>
2021-02-10 15:55:01 -0300

    feat: add CreateCompany use case and Company repositorie and entitie

---
+++ src/app/repositories/implementations/CompanyRepository.ts
@@ -0,0 +1,18 @@
+import { Company } from '../../entities/Company';
+import { ICompanyRepository } from '../ICompanyRepository';
+import { Company as CompanyModel } from '../../models/Company';
+
+export class CompanyRepository implements ICompanyRepository {
+  async findByCnpj(cnpj: string): Promise<Company | null> {
+    const company = await CompanyModel.findOne({
+      where: { cnpj: cnpj },
+    });
+
+    return company;
+  }
+
+  async save(company: Company): Promise<void> {
+    console.log(company);
+    await CompanyModel.create(company);
+  }
+}
---
+++ src/app/useCases/CreateCompany/CreateCompanyDTO.ts
@@ -0,0 +1,6 @@
+export interface ICreateCompanyRequestDTO {
+  name: string;
+  cnpj: string;
+  email: string;
+  password: string;
+}
