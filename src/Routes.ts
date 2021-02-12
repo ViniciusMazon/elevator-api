e598f0d53750d93e2731fcbc6d31fe3e7219d778
Vinicius Mazon <vnpmazon@gmail.com>
2021-02-12 10:24:15 -0300

    feat: add Authorization middleware

--- src/Routes.ts
+++ src/Routes.ts
@@ -3,6 +3,8 @@ import { createCompanyController } from './app/useCases/CreateCompany';
 import { createProfessionalController } from './app/useCases/CreateProfessional';
 import { authenticateController } from './app/useCases/Authenticate';
 
+import authorization from './app/middlewares/Authorization';
+
 class Routes {
   router = Router();
 
@@ -11,8 +13,7 @@ class Routes {
   }
 
   init() {
-    this.router.get('/', (request, response) => response.json({ ok: true }));
-
+    // Public routes
     this.router.post('/sing-up/company', (request, response) => {
       return createCompanyController.handle(request, response);
     });
@@ -24,6 +25,14 @@ class Routes {
     this.router.post('/authenticate', (request, response) => {
       return authenticateController.handle(request, response);
     });
+
+    // Private routes
+    this.router.get(
+      '/test',
+      (request, response, next) =>
+        authorization.handle(request, response, next),
+      (request, response) => response.json({ ok: true })
+    );
   }
 }
 
