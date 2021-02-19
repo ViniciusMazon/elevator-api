0bdd8d4dc148be775675a733d2e64a9adee20c4b
Vinicius Mazon <vnpmazon@gmail.com>
2021-02-19 18:37:49 -0300

    feat: add the findById method to ProfessionalRepository and OpportunitiesRepository

--- src/app/repositories/IOpportunitiesRepository.ts
+++ src/app/repositories/IOpportunitiesRepository.ts
@@ -1,5 +1,6 @@
 import { Opportunity } from '../entities/Opportunity';
 
 export interface IOpportunitiesRepository {
+  findById(id: string): Promise<Opportunity | null>;
   save(opportunity: Opportunity): Promise<void>;
 }
